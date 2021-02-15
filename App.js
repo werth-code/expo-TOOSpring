baa4d9b26434d266be9dce021de2213e0728576b
Matt Werth <matthewwerth@gmail.com>
2021-02-15 14:47:57 -0500

    Completed Simple ImageShare App

--- App.js
+++ App.js
@@ -1,15 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Image, Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'; 
+import uploadToAnonymousFilesAsync from 'anonymous-files'; 
+
 
 
 export default function App() {
+  
+ const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+ if (Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+    } 
+  };  
+
+   let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  }; 
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+       <Image source={{ uri: "https://docs.expo.io/static/images/tutorial/icon.png" }} style={styles.logo} />
        <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
         <TouchableOpacity
-        onPress={() => alert('Hello, world!')} style={styles.button}>
+        onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
@@ -44,4 +92,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
   }, 
+    thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
 });
